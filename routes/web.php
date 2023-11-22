ac5f453c480b750d6fd84dec4350b9f1e1b99a2f
aureliengremy <agremy@activix.ca>
2023-11-22 16:51:45 -0500

    User page UI

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
     return Inertia::render('Posts');
 });
 
+Route::get('/user', function () {
+    return Inertia::render('User');
+});
+
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
