d325e78dc0dde9d548efc1c2469bfac3ed1ea3ae
aureliengremy <agremy@activix.ca>
2024-02-22 17:09:26 -0500

    Add Resouces -> collection AllPostsCollection and function Index,Store,Destroy in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Http\Resources\AllPostsCollection;
 
 class PostController extends Controller
 {
@@ -13,54 +15,53 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Posts');
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return Inertia::render('Posts', [
+            'posts' => new AllPostsCollection($posts)
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(['text' => 'required']);
+        $post = new Post;
+
+        if ($request->hasFile('image')) {
+            $request->validate([
+                'image' => 'required|mimes:jpeg,png,jpg|max:2048',
+            ]);
+            $post = (new ImageService)->updateImage($post, $request);
+        }
+
+        $post->user_id = auth()->user()->id;
+        $post->text = $request->input('text');
+        $post->save();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
+     *  @params int $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(Post $post)
+
+    public function destroy(int $id)
     {
-        //
+        $post = Post::find($id);
+
+        if (!empty($post->image)) {
+            $currentImage = public_path() . $post->image;
+
+            if (file_exists($currentImage)) {
+                unlink($currentImage);
+            }
+        }
+
+        $post->delete();
     }
+
 }
