e3c0a07a7a6906cd4888a988b8e9b535950eb12e
aureliengremy <agremy@activix.ca>
2024-02-22 17:07:49 -0500

    add ImageService -> Intervention Image

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
+    | Services the application utilizes. Set this in your ".env" file.
     |
     */
 
@@ -150,7 +150,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
+    | request to your application. Feel free to add your own Services to
     | this array to grant expanded functionality to your applications.
     |
     */
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
